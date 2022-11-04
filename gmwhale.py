abc5b7eea582245e5cf68c6180cfc12b8f3998f5
n0wxJL <imbaplaydark@gmail.com>
2022-11-04 09:59:09 +0700

    ko

--- gmwhale.py
+++ gmwhale.py
@@ -1,5 +1,6 @@
 from http import client
 from binance.client import Client
+from songline import Sendline
 import time
 import datetime as dt
 from datetime import datetime
@@ -10,13 +11,17 @@ api_secret = tkk.api_secret
 token_noti = tkk.token_noti
 
 client = Client(api_key, api_secret)
-
+messenger = Sendline(token_noti)
+# nexttoday = (datetime.utcnow().date()) + dt.timedelta(days=1)
 def gmwhale():
     while True:
-        nexttoday = (datetime.utcnow().date()) + dt.timedelta(days=1)
         time_res = client.get_server_time()
         todaydate = datetime.fromtimestamp(time_res['serverTime']/1000).date()
-        if todaydate == nexttoday:
-            value_text = 'Good morning,Dolphin.'
+        if tdate is None:
+            tdate = todaydate
+        if tdate != todaydate:
+            tdate = todaydate
+            value_text = 'Good morning.'
             print(value_text)
+            messenger.sendtext(value_text)
         time.sleep(1)
