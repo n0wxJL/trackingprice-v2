26912137581483eeb2e759b9f9e8b383f909fea0
refeeln0w <novaxseldom@gmail.com>
2022-11-08 22:01:31 +0700

    ko

--- setup_var.py
+++ setup_var.py
@@ -1,6 +1,6 @@
 # gmwhale qoute
-txt_gm = ['Dont trust, Just verify','Hold on to your heart','And I will try to fix you','This is your moment, and every single minute you spend','Who cares if one more light goes out? Well, I do','There will be no white flag above my door','Not today','Lose Your self','Everybody want to rule the world']
+txt_gm = ['Dont trust, Just verify','Not your key, Not your coins','Hold on to your heart','And I will try to fix you','This is your moment, and every single minute you spend','Who cares if one more light goes out? Well, I do','There will be no white flag above my door','Not today','Lose Your self','Everybody want to rule the world']
 
 # kline interval
 interval = '1d'
-interval_candle = '1h'
+interval_candle = '1m'
