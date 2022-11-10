51293f550c09ea039bc91a9628fce84f42d9c284
n0wxJL <imbaplaydark@gmail.com>
2022-11-10 16:31:10 +0700

    ko

--- trackerprice.py
+++ trackerprice.py
@@ -10,8 +10,8 @@ def main():
     while True:
         if fn.time_next_day():
             gmwhale.gmwhale()
-        if fn.bar_time(sv.interval_candle,fn.time_server()):
-            pricetrack.pricetrack()
+        # if fn.bar_time(sv.interval_candle,fn.time_server()):
+        #     pricetrack.pricetrack()
         time.sleep(1)
 
 if __name__ == "__main__":
