f5e4bc7a9c7ad04bf8bafc00bdf574f237bf8719
n0wxJL <imbaplaydark@gmail.com>
2023-01-27 16:54:35 +0700

    ko

--- test.py
+++ test.py
@@ -43,7 +43,7 @@ fmt_min = '%Y-%m-%d %H:%M'
 # print(price.contents)
 
 
-# import yfinance as yf
+import yfinance as yf
 
 # tsla = yf.Ticker('TSLA')
 # print(tsla.history())
@@ -58,37 +58,21 @@ fmt_min = '%Y-%m-%d %H:%M'
 # frame.index = pd.to_datetime(frame.index, unit='ms')
 # frame = frame.astype(float)
 
-# frame['Date'] = pd.to_datetime(frame['Date'].dt.strftime('%Y-%m-%d'))
-# # print(frame.sort_values(by='Date',ascending=False))
-# print(frame['Date'][20])
+# def get_stock_price():
+#     print('get_stock_price()')
+#     alltext = '\n--Stocks--\n'
+#     for sym in coin_list.stockd:
+#         stk_pd = yf.Ticker(sym)
+#         print(stk_pd)
 
-# lookback = '300'
-# def get_report():
-#     print('get_report()')
-#     all_text = '\n--Report--\n'
-#     for sym in mycoin:
-#         df = fn.get_bar_data(sym,sv.interval,lookback)
-#         fn.applytechnical(df)
-#         # print(df)
-#         take_action = get_action_indicator(df)
-#         all_text = all_text + '{}\n  RSI: {:,.2f}\n  MACD: {:,.2f}\n  CDC: {:,.2f}\n{}-----------\n'.format(sym,df['rsi'][-2],df['macd'][-2],df['cdc'][-2],take_action)
-#     print(all_text)
-#     messenger.sendtext(all_text)
+#         frame = pd.DataFrame(stk_pd.history()).reset_index()
+#         frame = frame.iloc[:,:6]
+#         frame['Date'] = pd.to_datetime(frame['Date'].dt.strftime('%Y-%m-%d'))
+#         frame.sort_values(by='Date',ascending=True,inplace=True)
+#         stk_chg = ((frame['Close'][20] - frame['Close'][19])/frame['Close'][19])*100
+#         alltext += '{}: {:,.2f} CHG: {:,.2f}%\n.\n'.format(sym,frame['Close'][20],stk_chg)
+#     print(alltext)
+#     # messenger.sendtext(alltext)
 
-# def get_action_indicator(df):
-#     print('get_action_indicator()')
-#     print(df)
-#     alltext =''
-#     if (float(df['cdc'][-2])>0 and float(df['cdc'][-3]<0)):
-#         alltext = alltext + '=>CDC_BUY\n'
-#     elif (float(df['cdc'][-2])<0 and float(df['cdc'][-3]>0)):
-#         alltext = alltext +  '=>CDC_SELL\n'
-    
-#     if (float(df['rsi'][-1])>70):
-#         alltext = alltext + '=>RSI_OVERBOUGHT\n'
-#     elif(float(df['rsi'][-1])<30):
-#         alltext = alltext + '=>RSI_OVERSOLD\n'
-    
-#     return alltext
 
-# get_report()
+# get_stock_price()
