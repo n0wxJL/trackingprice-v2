f4074eb7adc26abab6d9c3b5ce8e5fa0d364bc92
n0wxJL <imbaplaydark@gmail.com>
2023-02-24 10:15:52 +0700

    ko

--- setup_var.py
+++ setup_var.py
@@ -1,5 +1,7 @@
 # gmwhale qoute
-txt_gm = ['Dont trust, Just verify','Not your key, Not your coins','Hold on to your heart','And I will try to fix you','This is your moment, and every single minute you spend','Who cares if one more light goes out? Well, I do','There will be no white flag above my door','Not today','Lose Your self','Everybody want to rule the world','1 BTC = 1 BTC','only 21 million','Every existing thing with reason','When my time comes forget the wrong that I ve done']
+txt_gm = ['Dont trust, Just verify','Not your key, Not your coins','Hold on to your heart','And I will try to fix you','This is your moment, and every single minute you spend','Who cares if one more light goes out? Well, I do',
+          'There will be no white flag above my door','Not today','Lose Your self','Everybody want to rule the world','1 BTC = 1 BTC','only 21 million','Every existing thing with reason','When my time comes forget the wrong that I ve done',
+          'Look up at the night sky,You''ll see the stars up there']
 
 # kline interval
 interval = '1d'
