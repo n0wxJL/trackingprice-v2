5b55948b15c359486d3d79b1db4d571bb3278d36
n0wxJL <imbaplaydark@gmail.com>
2023-03-16 15:07:09 +0700

    ko

--- coin_list.py
+++ coin_list.py
@@ -1,6 +1,6 @@
 mycoin = ['BTCUSDT','BTCBUSD','ETHUSDT','ETHBUSD']
 
 #for list report 
-coin_list_tf = ['BTC-USD','ETH-USD','ZCX-USD','SNX-USD','QNT-USD','DOGE-USD']
+coin_list_tf = ['BTC-USD','ETH-USD','ZCX-USD','SNX-USD','OP-USD','DOGE-USD','LQTY-USD']
 
 stockd = ['TSLA','AAPL','GOOG','BDMS.BK','AMA.BK','VL.BK','RSP.BK','CPALL.BK','LH.BK']
