06b3eba0caa9b0ddc382f85393646e042e27eb0f
refeeln0w <novaxseldom@gmail.com>
2023-04-01 17:34:15 +0700

    ko

--- token_api.py
+++ token_api.py
@@ -1,5 +1,5 @@
 # token Line 'Notify Test Bot'
 token_noti = 'qLyPnPXQ8LUXGhOORTaS9qgsEqxAsnktH5f0bL7YXf5'
 #api key
-api_key = 'ZfabUgJUVCzbHsRqm8hrtiCWeEXGD3eWra61sDKg0gEWT4H7yRlgFvMs3DRYd9Ca'
-api_secret = '3dM8yBct5Gw6ABvXnJAce4AcN6o5N9qT0YYSPASwA1TjSmf35oaNeYLwoyB0VIUw'
+api_key = ''
+api_secret = ''
