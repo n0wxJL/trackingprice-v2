1d39b5f66e8f58fc923c97e4e89956c9c9885d50
refeeln0w <novaxseldom@gmail.com>
2023-04-17 22:49:01 +0700

    change bug bar time

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ import fn_stock
 
 def main():
     # add call fn between the day
-    bar_time = fn.genTimeInterval()
     while True:
+        bar_time_len = 0
+        if bar_time_len <= 2:
+            bar_time = fn.genTimeInterval()
+            bar_time_len = len(bar_time)
         if fn.time_next_day():
             gmwhale.gmwhale()
             fn.delay(60)
@@ -19,13 +22,11 @@ def main():
             fn.delay(2)
         
         timenow = fn.datetimeUtcNow()
-        bar_time_len = len(bar_time)
         for i in range(bar_time_len):
             if (timenow >= bar_time[i]) and (timenow < bar_time[i+1]):
                 pricetrack.pricetrack()
                 bar_time.pop(i)
                 break
-
         fn.delay(1)
 
 if __name__ == "__main__":
