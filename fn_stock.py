38d04e6056763ed1230482da32ed4a4616557f3f
refeeln0w <novaxseldom@gmail.com>
2023-05-20 17:12:46 +0700

    ko

--- fn_stock.py
+++ fn_stock.py
@@ -103,7 +103,7 @@ def topyield():
     all_text = '\nTop Yield'
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36'}
     r = requests.get(url,headers=headers)
-    print(r.status_code)
+    print('status site:',r.status_code)
     browser = load_chrome_driver('')
     browser.get(url)
     html = browser.page_source
